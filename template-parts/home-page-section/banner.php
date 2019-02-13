8f45976eb0096b1bca366ec98dd5bdab1de643aa
Abhishek <abhiverma.abhishek@gmail.com>
2019-02-13 12:05:46 +0530

    changed homepage banner to full screen

--- template-parts/home-page-section/banner.php
+++ template-parts/home-page-section/banner.php
@@ -1,7 +1,7 @@
 <section class="banners">
     <div class="container-fluid">
         <div class="row banner-background-color">
-            <div class="col-md-9 col-sm-12">
+            <div class="col-md-12 col-sm-12 no-padding">
                 <?php 
                     if( get_field('banner_image') ) { 
                         ?>
@@ -12,22 +12,22 @@
                     } 
                 ?>
             </div>
-            <div class="col-md-3 col-sm-12 home_icon_none_mobile">
-                <?php
-                    if( have_rows('banner_icons') ) {
-                        while ( have_rows('banner_icons') )  {
-                            the_row(); 
-                            ?>
-                            <div class="row beside_banners">
-                                <div class="col-md-12">
-                                    <img src="<?php the_sub_field('icon'); ?>"class="img-responsive banner-icon-image" alt=""> <p><?php the_sub_field('description'); ?></p>
-                                </div>                                
-                            </div>
-                            <?php  
-                        }
-                    }
-                ?>
-            </div>
+<!--            <div class="col-md-3 col-sm-12 home_icon_none_mobile">-->
+<!--                --><?php
+//                    if( have_rows('banner_icons') ) {
+//                        while ( have_rows('banner_icons') )  {
+//                            the_row();
+//                            ?>
+<!--                            <div class="row beside_banners">-->
+<!--                                <div class="col-md-12">-->
+<!--                                    <img src="--><?php //the_sub_field('icon'); ?><!--"class="img-responsive banner-icon-image" alt=""> <p>--><?php //the_sub_field('description'); ?><!--</p>-->
+<!--                                </div>                                -->
+<!--                            </div>-->
+<!--                            --><?php //
+//                        }
+//                    }
+//                ?>
+<!--            </div>-->
         </div>
     </div>
 </section>
