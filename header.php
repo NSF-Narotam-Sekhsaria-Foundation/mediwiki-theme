bb2bfb43e9910a95447f8cbda5eaffecabe545d8
Abhishek <abhiverma.abhishek@gmail.com>
2019-04-19 14:39:28 +0530

    added styling for marathi

--- header.php
+++ header.php
@@ -80,30 +80,9 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   	<?php
-//                    $raw_language = pll_the_languages(array('raw' => 1));
-//
-//                    $languages = pll_languages_list();
-//                    foreach ($languages as $language) {
-//                        //if is not current language
-//                        if (pll_current_language() != $language) {
-//                            if (!$raw_language[$language]['no_translation']) {
-//                                $raw_language[$language]['url'];
-//                                $language_set = '<a href ="'. $raw_language[$language]['url'].'" style="color: #3479c3;">';
-//                                $language_set .= $raw_language[$language]['name'];
-//                                $language_set .= '</a>';
-//                            }
-//                        }
-//                        //and if is translation available
-//                        // add to the menu
-//                    }
-
-                    $items_wrap = '<ul id="%1$s" class="%2$s">%3$s';
-                    $items_wrap .= '<li id="language-switcher">'.$language_set.'</li>';
-                    $items_wrap .= '</ul>';
                     wp_nav_menu( array(
                   		'theme_location' => 'menu-1', 
-                  		'menu_class' => 'nav navbar-nav navbar-right desktop-menu',
-                        'items_wrap' => $items_wrap
+                  		'menu_class' => 'nav navbar-nav navbar-right desktop-menu'
                     ) );
                   	?>
 
