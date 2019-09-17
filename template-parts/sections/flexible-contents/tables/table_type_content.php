6b03dfdb7e1f0c3a5ae41423a68cfec1eafb4a7e
Abhishek Verma <abhi11verma@gmail.com>
2019-09-17 12:14:48 +0530

    Adds: Feature to adjust dynamic column widht

--- template-parts/sections/flexible-contents/tables/table_type_content.php
+++ template-parts/sections/flexible-contents/tables/table_type_content.php
@@ -9,14 +9,21 @@
                     ?>
                     <h3 class="content-heading"><?php the_sub_field('table_heading_table_type_content'); ?></h3>
                     <?php 
-                } 
+                }
+                if(get_sub_field('table_width_col1')!=0 && get_sub_field('table_width_col2')!=0){
+                    $width_col1 = get_sub_field('table_width_col1')."%";
+                    $width_col2 = get_sub_field('table_width_col2')."%";
+                }else{
+                    $width_col1 = "";
+                    $width_col2 = "";
+                }
             ?>
             <div class="col-md-12 padding-left-0">
                 <table class="table table-responsive custom-table">
                     <thead>
                         <tr>             
-                            <th class="col-md-3"><?php the_sub_field('table_column_heading_1'); ?></th>
-                            <th class="col-md-9"><?php the_sub_field('table_column_heading_2'); ?></th>                            
+                            <th width="<?php echo $width_col1; ?>" class="col-md-3"><?php the_sub_field('table_column_heading_1'); ?></th>
+                            <th width="<?php echo $width_col2; ?>" class="col-md-9"><?php the_sub_field('table_column_heading_2'); ?></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -31,7 +38,7 @@
                                             while( have_rows('content_group_table_type_content') ) {
                                                 the_row();
                                                 ?>                                        
-                                                <td>
+                                                <td width="<?php echo $width_col1; ?>">
                                                     <?php $heading_inside_table_type_content = get_sub_field('heading_inside_table_type_content');
                                                         if ($heading_inside_table_type_content!="") {
                                                             ?>
@@ -53,7 +60,7 @@
                                             }
                                         }
                                     ?>
-                                        <td style="padding-left: 15px!important;">
+                                        <td width="<?php echo $width_col2;?>" style="padding-left: 15px!important;">
                                             <?php $content_inside_table_type_content_copy = get_sub_field('content_inside_table_type_content_copy');
                                                 if ($content_inside_table_type_content_copy!="") {
                                                     ?>
